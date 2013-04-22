a112727ede31ef62df864a57bc7a0de7f477d1b1
Gregory Brown <gregory.t.brown@gmail.com>
2013-04-22 15:50:32 -0400

    Minor tweaks

--- v6/009/lib/spyglass/link_fetcher/reddit.rb
+++ v6/009/lib/spyglass/link_fetcher/reddit.rb
@@ -5,10 +5,11 @@ require_relative "../data/link"
 module Spyglass
   module LinkFetcher
     Reddit = ->(subreddit) do
-      uri = "http://api.reddit.com/r/#{subreddit}?limit=100"
+      document = open("http://api.reddit.com/r/#{subreddit}?limit=100").read
 
-      JSON.parse(open(uri).read)["data"]["children"].map do |e|
+      JSON.parse(document)["data"]["children"].map do |e|
         e = e["data"]
+
         Data::Link.new(url: e["url"], score: e["score"], title: e["title"])
       end
     end
