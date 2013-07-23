9474e441cbf03135aadc5c461b2c5c8d110da1a4
Jia Brown <wujia123@yahoo.com>
2013-07-23 11:49:37 -0400

    modified q3 plot

--- v7/003/q3.R
+++ v7/003/q3.R
@@ -4,12 +4,22 @@ library("TTR")
 file <- download.file("http://sleepy-shore-7394.herokuapp.com/mood-logs.csv", destfile="mood-logs.csv")
 
 data <- read.table("mood-logs.csv",header=FALSE,sep=",")
+names(data)[1]<-"time"
+names(data)[2]<-"rating"
+names(data)[3]<-"day"
 
-data_mean_EMA <- EMA(data$V2,n=20)
-data_mean <- mean(data$V2)
-data_sd <- sd(data$V2)
+data_mean_EMA <- EMA(data$rating,n=20)
+data_mean <- mean(data$rating)
+data_sd <- sd(data$rating)
 
-plot(data$V1,data_mean_EMA,type="l",col="red",xaxt="n",ylim=c(1,9))
+#not accurate, but probably looks good enough
+x_at<-aggregate(data$time,by=list(data$day),FUN=mean)
+x_label<-aggregate(data$day,by=list(data$day),FUN=mean)
+  
+plot(data$time,data_mean_EMA,type="l",col="red",xaxt="n",ylim=c(1,9),xlab="day",
+     ylab="Moving Average of Rating")
+
+axis(side=1,at=x_at$x,labels=x_label$x)
 
 abline(h=data_mean,col="green")
 abline(h=data_mean+data_sd,col="gray")
