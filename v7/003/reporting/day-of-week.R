a1aebba5df9f1602a903f8c1148a631ffa9d0b97
Gregory Brown <gregory.t.brown@gmail.com>
2013-07-28 17:52:22 -0400

    Fairly clean report (visually)

--- v7/003/reporting/day-of-week.R
+++ v7/003/reporting/day-of-week.R
@@ -15,7 +15,7 @@ data_day_sd <- aggregate(data$rating, by=list(data$weekdaynum),FUN=sd)
 
 draw_jpg("day-of-week-summary", function() {
   plot(data_day_mean$Group.1,data_day_mean$x,xlim=c(0,6), ylim=c(1,9),type="o",
-       xaxt="n",xlab="Day of the Week",ylab="Mood rating",yaxt="n",
+       xaxt="n",xlab="Day of week",ylab="Mood rating",yaxt="n",
        main="Average mood by day of week", cex.lab=1.5, cex.main=2, lwd=3)
   axis(side=1,at=c(0:6),label=week, cex.axis=1.5)
   axis(side=2, at=c(1:9), cex.axis=1.5)
